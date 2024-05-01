5303f2fce95befb96f89617bc34697cb5fbb589b
Quentin <quentinmet7@gmail.com>
2024-05-01 17:23:32 +0200

    Findu cours 01.05.2024

---
+++ RotateTranslate/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RotateTranslate
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RotateTranslate/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace RotateTranslate
+{
+    public partial class MainPage : ContentPage
+    {
+        private int speed = 1;
+        private string label = "Rotate 90°";
+        public MainPage()
+        {
+            InitializeComponent();
+
+            slider.Value = speed;
+
+            lbox.Text = label;
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (label == "Rotate 90°") {
+                this.box.RotateTo(90, 2000);
+                label = "Rotate Back";
+            } else {
+                this.box.RotateTo(0, 2000);
+                label = "Rotate 90°";
+            }
+
+            lbox.Text = label;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.box.TranslateTo(box.X, box.Y);
+        }
+    }
+
+}
